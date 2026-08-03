5075273d377e669cf94cb1b180c42d260749a51b
emmanuelbrian257-star <emmanuelbrian257@gmail.com>
2026-08-03 19:42:50 +0300

    Update ThemeProvider.jsx

--- app/components/ThemeProvider.jsx
+++ app/components/ThemeProvider.jsx
@@ -1,13 +1,13 @@
 "use client"
 
-import React, {ReactNode, useState, useMemo} from "react"
+import React, {useState, useMemo} from "react"
 import {createTheme} from "@mui/material/styles"
 import {ThemeProvider, CssBaseline} from "@mui/material"
 import {useSelector} from "react-redux"
 import {themeSettings} from "../../constants/theme"
 
 
-const MainLayout=({children}:{children:ReactNode})=>{
+const MainLayout=({children})=>{
     const mode=useSelector((state)=>state.global.mode)
     const theme=useMemo(()=>createTheme(themeSettings(mode)),[mode])
     return(
