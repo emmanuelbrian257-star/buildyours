f9be87db6261997c04c1e41ae58354b9dfc396f1
emmanuelbrian257-star <emmanuelbrian257@gmail.com>
2026-08-06 10:55:37 +0300

    Update page.tsx

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 "use client"
 import React, { useState } from 'react'
 import {Box,useTheme, useMediaQuery} from "@mui/material"
